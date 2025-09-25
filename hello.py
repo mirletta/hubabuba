2226784a7e234e2178a05eae336928bd243d1ce6
unknown <i201-203@VLAN-25-brb-096.rd.mirea.ru>
2025-09-25 13:41:21 +0300

    hello

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print (1)
+import shlex
+while True:
+    a = input("vfs@")
+    b = shlex.split(a)
+    if a == "exit":
+        break
+    if len(b) == 0:
+        continue 
+    if b[0] == "ls":
+        print(b)
+    elif b[0] == "cd":
+        print(b)
+    else:
+        print(f'{b[0]}): command not found')
