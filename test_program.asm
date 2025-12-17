04c6180cd81c51992391c13097965b228254d0f1
mirletta <milolikakarpova@gmail.com>
2025-12-17 10:36:58 +0300

    Create test_program.asm

---
+++ test_program.asm
@@ -0,0 +1,18 @@
+; Загрузка констант
+LOAD 1 123
+LOAD 2 456
+LOAD 3 789
+
+; Запись в память
+WRITE 1 100
+WRITE 2 101
+WRITE 3 102
+
+; Чтение из памяти
+READ 4 1 0
+READ 5 2 0
+READ 6 3 0
+
+; Bit reverse операция
+LOAD 7 0x12345678
+BITREV 7 200
